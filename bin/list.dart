98f3248f82866327b7bf21ab2fab62c925986392
a-saber <ahs629862@gmail.com>
2026-08-08 12:42:15 +0300

    high

--- bin/list.dart
+++ bin/list.dart
@@ -1,27 +1,37 @@
 class Car{}
 void main(){
-  List<int> l = [1 , 2, 3, 4, 5];
-  l.add(6);
-  l.addAll([7, 8, 9, 3]);
-  l.insert(1, -1);
-  l.insertAll(3, [-2, -3]);
-  print(l);
-  print(l.remove(30));
-  // l.removeWhere()
-  l[0] = 100;
-  print(l);
+  List<int> l = [-1, -2, -3, 0, 1 , 2, 3, 4, 5];
+    l.removeWhere(
+      (x)=> x.isNegative
+    );
+    print(l);
+  l=  l.map(
+    (e)=> e*2
+  ).toList();
+    print(l);
+    // l.firstWhere(test)
 
-  for(int i=0; i<l.length; i++){
-    l[i] = l[i] * 2;
-  }
-  print(l);
+  // l.add(6);
+  // l.addAll([7, 8, 9, 3]);
+  // l.insert(1, -1);
+  // l.insertAll(3, [-2, -3]);
+  // print(l);
+  // print(l.remove(30));
+  // // l.removeWhere()
+  // l[0] = 100;
+  // print(l);
 
-  l.forEach(
-    (int element){
-      print(element);
-    }
-  );
+  // for(int i=0; i<l.length; i++){
+  //   l[i] = l[i] * 2;
+  // }
+  // print(l);
 
-  // l.removeWhere(test)
+  // l.forEach(
+  //   (int element){
+  //     print(element);
+  //   }
+  // );
+
+
 
 }
