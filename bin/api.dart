9810c6cfd5c641bea8c437bb59bb1f3dcf7b3710
a-saber <ahs629862@gmail.com>
2026-09-04 12:15:29 +0300

    handle exception

--- bin/api.dart
+++ bin/api.dart
@@ -1,24 +1,59 @@
-
 import 'package:dio/dio.dart';
-
-void main()async{
   Dio dio = Dio();
-  Map<String, dynamic> requestData = {
-    "username": 'ahmed01',
-    "password": '1234567'
-  };
-try{
-    var response = await dio.post(
-    "https://ntitodo-production-cddf.up.railway.app/api/login",
-    data: FormData.fromMap(requestData),
-    );
 
-    print(response.toString());
+void main() async {
+  await register();
 }
-catch(e){
-  print(e.toString());
-  if(e is DioException){
-    print(e.response?.data.toString());
+
+register()async
+{
+  try{
+    var response = await dio.post(
+      'https://ntitodo-production-cddf.up.railway.app/api/register',
+      data: FormData.fromMap({
+        'username': 'ahmed007',
+        'password': '123456',
+      })
+    );
+    print(response.data.toString());
   }
+   catch(e){
+    handleException(e);
+   }
 }
+weather()async{
+  
+   try{
+     var response = await dio.get(
+    'https://api.openweathermap.org/data/2.5/weather',
+    queryParameters: {
+      'lat': 30.1234,
+      'lon': 31.1234,
+      'appid': '39ef56aa87e0f9d833e66cd9111de959',
+      'units': 'metric'
+    }
+  );
+
+  print(response.data.toString());
+   }
+   catch(e){
+    handleException(e);
+   }
+}
+
+void handleException(Object e){
+  
+    if(e is DioException){
+      if(e.response?.data != null){
+        var errorResponse = e.response?.data as Map<String, dynamic>;
+        print(errorResponse['message']);
+      }
+      else{
+        print('Network error happened try again later');
+      }
+      
+    }
+    else{
+      print('error happened try again later');
+    }
 }
--- bin/map.dart
+++ bin/map.dart
@@ -1,3 +1,28 @@
 void main(){
+
+  Map<int, String> m = {
+    1 : 'ahmed',
+    2 : 'karim',
+    3 : 'mahmoud',
+    4: 'mostafa'
+  };
+
+  m.addAll({
+    1 : 'ahmed01',
+    5 : 'ali',
+    6 : 'mohamed'
+  });
+
+  m[10] = 'Ali';
+
+  print(m.remove(1));
+
   
+  
+  print(m);
+  print(m[66]?.contains('h'));
+
 }
+class Car{}
+  
+
